24a7497fa6dbee00f86ab104c92f859b7f98f0f3
Hagin Onyango <hagincubes@gmail.com>
2016-05-25 18:51:07 -0400

    Updated from Brython Server: 5/25/2016 6:51:06 PM

--- code.py
+++ code.py
@@ -41,3 +41,16 @@ def showInstructions():
     print("\n 0 " + sep + " 1 " + sep + " 2 ");
     print(" 3 " + sep + " 4 " + sep + " 5 ");
     print(" 6 " + sep + " 7 " + sep + " 8 \n");
+
+def checkWin(b, h, c):
+    combinations = ([0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]):
+        for comb in combinations:
+            if(b[comb[0]] == h and b[comb[1]] == h and b[comb[2]] == h):
+                return "human";
+            if(b[comb[0]] == c and b[comb[1]] == c and b[comb[2]] == c):
+                return "computer";
+        return "no";
+        
+def drawBoard(b, h, c):
+    sep = " | ";
+    print("\n"+b[0]+sep+b[1]+sep+b[2]);
