6819d4e349e32983e75c35675744393849f241ff
Hagin Onyango <hagincubes@gmail.com>
2016-06-08 19:42:31 -0400

    Update code.py

--- code.py
+++ code.py
@@ -7,3 +7,14 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 #define the print_board function 
 def print_header():
+	print"""
+ _____  _  ____     _____  ____  ____     _____  ____  _____
+/__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/    1 | 2 | 3
+  / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \      4 | 5 | 6
+  | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_     7 | 8 | 9
+  \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\
+                                                            
+ To play Tic-Tac-Toe, you need to get three in a row...
+ Your choices are defined, they must be from 1 to 9...
+
+"""
