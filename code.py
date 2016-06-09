f47fa92937ae7a76ad47e4558942f9a6a5d18b47
Hagin Onyango <hagincubes@gmail.com>
2016-06-08 21:02:12 -0400

    Updated from Brython Server: 6/8/2016 9:02:11 PM

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 #define the print_board function 
 def print_header():
-	print"""
+	print("""
  _____  _  ____     _____  ____  ____     _____  ____  _____
 /__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/    1 | 2 | 3
   / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \      4 | 5 | 6
@@ -17,4 +17,31 @@ def print_header():
  To play Tic-Tac-Toe, you need to get three in a row...
  Your choices are defined, they must be from 1 to 9...
 
-"""
+""")
+
+#Define the print_board function
+def print_board():
+    print("   |   |   ")
+    print(" "+board[1]+" | "+board[2]+" | "+board[3]+"  ")
+    print("   |   |   ")
+    print("---|---|---")
+    print("   |   |   ")
+    print(" "+board[4]+" | "+board[5]+" | "+board[6]+"  ")
+    print("   |   |   ")
+    print("---|---|---")
+    print("   |   |   ")
+    print(" "+board[7]+" | "+board[8]+" | "+board[9]+"  ")
+    print("   |   |   ")
+    print("---|---|---")
+    print("   |   |   ")
+    
+while True:
+    os.system("clear")
+    print_header()
+    print_board()
+    
+    choice = input("please choose an empty space for X. ")
+    choice = int(choice)
+    
+    board[choice] = "X"
+    
