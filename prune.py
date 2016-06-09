2c26fd2068f6193c609a887b661e84d324fb8963
Hagin Onyango <hagincubes@gmail.com>
2016-06-09 16:07:24 -0400

    Updated from Brython Server: June 9, 2016 4:07:25 PM EDT

--- prune.py
+++ prune.py
@@ -1,1 +1,17 @@
-
+def isFull(l):
+    for x in l: 
+        if x == "":
+            return False
+    return True
+    
+def prune(l):
+    for a in range(0, 2):
+        if l[0+a] == l[3+a] == l[6+a] == "x":
+            return False
+        if l[3*a + 0] == l[3*a + 1] == l[3*a+ 2] == "x":
+            return False 
+    if l[0] == l[4] == l[8] == "x":
+        return False
+    if l[2] == l[4] == l[6] == "x":
+        return False 
+    return True 
