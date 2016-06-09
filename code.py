45af54359a3cb29c287e659678df41de5ec455df
Hagin Onyango <hagincubes@gmail.com>
2016-06-08 21:43:39 -0400

    Updated from Brython Server: 6/8/2016 9:43:39 PM

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import time
 import random 
 
 #define the board 
-board = [" "," "," "," "," "," "," "," "," "," "]
+board = [""," "," "," "," "," "," "," "," "," "]
 
 #define the print_board function 
 def print_header():
