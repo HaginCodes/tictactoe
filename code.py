82f406c0dd73399a80316273dba3508b967b4b26
Hagin Onyango <hagincubes@gmail.com>
2016-06-08 21:22:55 -0400

    Updated from Brython Server: 6/8/2016 9:22:54 PM

--- code.py
+++ code.py
@@ -49,7 +49,14 @@ while True:
     
     
     #check for X win
-    if(board[1] == "X" and board[2] =="X" and board[3] == "X"):
+    if(board[1] == "X" and board[2] =="X" and board[3] == "X") or\
+       (board[4] == "X" and board[5] =="X" and board[6] == "X")or\
+       (board[7] == "X" and board[8] =="X" and board[9] == "X")or\
+       (board[1] == "X" and board[4] =="X" and board[7] == "X")or\
+       (board[2] == "X" and board[5] =="X" and board[8] == "X")or\
+       (board[3] == "X" and board[6] =="X" and board[9] == "X")or\
+       (board[1] == "X" and board[5] =="X" and board[9] == "X")or\
+       (board[3] == "X" and board[5] =="X" and board[7] == "X"):
         os.system("clear")
         print_header()
         print_board()
