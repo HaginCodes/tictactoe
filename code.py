fc52e2e044beab95f6cf68f1655ae5d212e3c4b5
Hagin Onyango <hagincubes@gmail.com>
2016-06-09 23:03:57 -0400

    Updated from Brython Server: 6/9/2016 11:03:56 PM

--- code.py
+++ code.py
@@ -34,19 +34,30 @@ def print_board():
     print("   |   |   ")
     
     
-def is_x_winnner(board):
-      if(board[1] == "X" and board[2] =="X" and board[3] == "X") or\
-       (board[4] == "X" and board[5] =="X" and board[6] == "X")or\
-       (board[7] == "X" and board[8] =="X" and board[9] == "X")or\
-       (board[1] == "X" and board[4] =="X" and board[7] == "X")or\
-       (board[2] == "X" and board[5] =="X" and board[8] == "X")or\
-       (board[3] == "X" and board[6] =="X" and board[9] == "X")or\
-       (board[1] == "X" and board[5] =="X" and board[9] == "X")or\
-       (board[3] == "X" and board[5] =="X" and board[7] == "X"):
+def is_x_winner(board):
+    if(board[1] == "X" and board[2] =="X" and board[3] == "X") or\
+        (board[4] == "X" and board[5] =="X" and board[6] == "X")or\
+        (board[7] == "X" and board[8] =="X" and board[9] == "X")or\
+        (board[1] == "X" and board[4] =="X" and board[7] == "X")or\
+        (board[2] == "X" and board[5] =="X" and board[8] == "X")or\
+        (board[3] == "X" and board[6] =="X" and board[9] == "X")or\
+        (board[1] == "X" and board[5] =="X" and board[9] == "X")or\
+        (board[3] == "X" and board[5] =="X" and board[7] == "X"):
+        return True
+    else:
+        return False
+def is_0_winner(board):
+    if(board[1] == "O" and board[2] =="O" and board[3] == "O") or\
+       (board[4] == "O" and board[5] =="O" and board[6] == "O")or\
+       (board[7] == "O" and board[8] =="O" and board[9] == "O")or\
+       (board[1] == "O" and board[4] =="O" and board[7] == "O")or\
+       (board[2] == "O" and board[5] =="O" and board[8] == "O")or\
+       (board[3] == "O" and board[6] =="O" and board[9] == "O")or\
+       (board[1] == "O" and board[5] =="O" and board[9] == "O")or\
+       (board[3] == "O" and board[5] =="O" and board[7] == "O"):
            return True
-        else:
-            return False
-    
+    else:
+        return False
     
     
     
@@ -61,7 +72,7 @@ while True:
     
     #check to see if space is empty first
     if board[choice] == " ":
-        board[choice] ="X"
+        board[choice] = "X"
     else:
         print("Sorry,that space is not empty!")
         time.sleep(1)
@@ -102,14 +113,7 @@ while True:
     
     
     #check for X win
-    if(board[1] == "O" and board[2] =="O" and board[3] == "O") or\
-       (board[4] == "O" and board[5] =="O" and board[6] == "O")or\
-       (board[7] == "O" and board[8] =="O" and board[9] == "O")or\
-       (board[1] == "O" and board[4] =="O" and board[7] == "O")or\
-       (board[2] == "O" and board[5] =="O" and board[8] == "O")or\
-       (board[3] == "O" and board[6] =="O" and board[9] == "O")or\
-       (board[1] == "O" and board[5] =="O" and board[9] == "O")or\
-       (board[3] == "O" and board[5] =="O" and board[7] == "O"):
+    if is_o_winner(board):
         os.system("clear")
         print_header()
         print_board()
