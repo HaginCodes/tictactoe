19e9da2b84d7d2e7ae3950a1b705b7ddf5090565
Hagin Onyango <hagincubes@gmail.com>
2016-06-11 08:35:53 -0400

    Unbeatable Tic Tac Toe

--- code.py
+++ code.py
@@ -8,11 +8,11 @@ board = [""," "," "," "," "," "," "," "," "," "]
 #define the print_board function 
 def print_header():
 	print("""
-	             _____  _  ____     _____  ____  ____     _____  ____  _____
-	            /__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/    
-	              / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \      
-	              | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_     
-	              \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\
+     _____  _  ____     _____  ____  ____     _____  ____  _____
+    /__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/    
+             / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \      
+             | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_     
+             \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\
                                                                         
              To play Tic-Tac-Toe, you need to get three in a row...
              Your choices are defined, they must be from 1 to 9...
@@ -71,42 +71,45 @@ def get_computer_move(board, player):
     #if the center square is empty choose that
     if board[5] == " ":
         return 5
-    else:
-        if board[7] == " ":
-		return 7
 
     while True:
         move = random.randint(1,9)
         # move is blank go and return otherwise try again
         if board[move] == " ":
             return move
-            break
-        
-    return 5
+    
 
 def play_defense(board, player):
     
-
+    #columns
     for i in [1,2,3]:
-        if board[i] == player and board[i+3] == player and board[i+6] == " ":
+        if board[i] == "X" and board[i+3] == "X" and board[i+6] == " ":
             return i+6
-        if board[i+3] == player and board[i+6] == player and board[i] == " ":
+        if board[i+3] == "X" and board[i+6] == "X" and board[i] == " ":
             return i
-        if board[i] == player and board[i+6] == player and board[i+3] == " ":
+        if board[i] == "X" and board[i+6] == "X" and board[i+3] == " ":
             return i+3
-    
-    if board[1] == player and board[5] == player and board[9] == " ":
+    #rows
+    for a in [1,4,7]:
+        if board[a] == "X" and board[a+1] == "X" and board[a+2] == " ":
+            return a+2
+        if board[a+1] == "X" and board[a+2] == "X" and board[a] == " ":
+            return a
+        if board[a+2] == "X" and board[a] == "X" and board[a+1] == " ":
+            return a+1
+
+    if board[1] == "X" and board[5] == "X" and board[9] == " ":
        return 9	
-    if board[5] == player and board[9] == player and board[1] == " ":
+    if board[5] == "X" and board[9] == "X" and board[1] == " ":
        return 1	
-    if board[9] == player and board[1] == player and board[5] == " ":
+    if board[9] == "X" and board[1] == "X" and board[5] == " ":
        return 5	
-    
-    if board[3] == player and board[5] == player and board[7] == " ":
+
+    if board[3] == "X" and board[5] == "X" and board[7] == " ":
        return 7
-    if board[5] == player and board[7] == player and board[3] == " ":
+    if board[5] == "X" and board[7] == "X" and board[3] == " ":
        return 3
-    if board[7] == player and board[3] == player and board[5] == " ":
+    if board[7] == "X" and board[3] == "X" and board[5] == " ":
        return 5
     
         
